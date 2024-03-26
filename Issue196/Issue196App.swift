0d24b058189173e0c2a1f4c72953f6921a61845d
Steven G. Harris <steven.g.harris@gmail.com>
2024-03-26 09:05:28 -0700

    Initial Commit

---
+++ Issue196/Issue196App.swift
@@ -0,0 +1,17 @@
+//
+//  Issue196App.swift
+//  Issue196
+//
+//  Created by Steven Harris on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Issue196App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
